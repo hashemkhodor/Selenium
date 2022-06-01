05cb814094e09d552861f8a97041bd26943acbcb
hashemkhodor <87622346+hashemkhodor@users.noreply.github.com>
2022-06-01 13:02:14 +0300

    Add files via upload

---
+++ ScrapeBot/MainAmazonScraping.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 21 17:29:54 2022
+
+@author: Hashem
+"""
+
+from AmazonBot import AmazonBot
+
+Bot1= AmazonBot('C:\SeleniumWebdrivers\chromedriver_win32 (6)\chromedriver.exe')
+Bot1.scrapeproduct("sunglasses")
+#Bot1.exportxlsx()
