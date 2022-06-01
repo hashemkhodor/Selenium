e646f261219fecba33d6b8bd7e7b1e487b9064a8
hashemkhodor <87622346+hashemkhodor@users.noreply.github.com>
2022-06-01 18:11:23 +0300

    Add files via upload

---
+++ constants.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 22 15:56:37 2022
+
+@author: Hashem
+"""
+
+productname='span[id="productTitle"]'
+price='span[class="a-price-range"] [class="a-offscreen"] , span[id="price_inside_buybox"] , span[data-a-color="price"]'
+search_bar='input[id="twotabsearchtextbox"]'
